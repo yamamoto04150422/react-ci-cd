e022b9b505035247afaf1d33978f587e229c4834
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-06-01 23:02:23 +0900

    正常系

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,11 @@
 import { describe, it }from 'vitest'
-import { render, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
   it('sample test', () => {
-    const { getByText } = render(<App />);
-  const button = getByText(/count iws 0/i);
-  fireEvent.click(button);
-
+    render(<App />)
+    
+    screen.debug();
   })
 })
