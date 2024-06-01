6a7325771a37ea11707c03f01c80d51e4c588069
山本勇太 <yamamotoyuuta@yamamotoyuutanoMacBook-Pro.local>
2024-06-01 23:00:38 +0900

    test_check

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,12 @@
 import { describe, it }from 'vitest'
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import App from './App'
 
 describe('App', () => {
   it('sample test', () => {
     const { getByText } = render(<App />);
-  const button = getByText(/count is 0/i);
+  const button = getByText(/count iws 0/i);
   fireEvent.click(button);
-  expect(button).toHaveTextContent("count is 1");
 
   })
 })
